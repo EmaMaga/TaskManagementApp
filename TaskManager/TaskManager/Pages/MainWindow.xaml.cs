9c615b18b0edba7539042e961ae21856632736a4
EmaMaga <135460319+EmaMaga@users.noreply.github.com>
2024-03-15 23:47:38 -0300

    Add files via upload
    Aqui estaria la aplicacion .net WPF TaskManagmentApp.
    
    De momento solo se encuentra serarrollado la interfaz de LogIn y Register, pero en los proximos dias estare actualizando el repositorio con las demas funcionalidades faltantes.

---
+++ TaskManager/TaskManager/Pages/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace TaskManager {
+    public partial class MainWindow : Window
+    {
+        String StringConnection = "TaskManager.Properties.Settings.Task_ManagerConnectionString";
+        SqlConnection DataConnection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            String ConnectionStrig = ConfigurationManager.ConnectionStrings[StringConnection].ConnectionString;
+            DataConnection = new SqlConnection(ConnectionStrig);
+        }
+
+     private void Login_Click(object sender, RoutedEventArgs e)
+     {
+        String Name = this.UserName.Text;
+        String Pass = this.Password.Password;
+        if(CheckLogin(Name,Pass)==true)
+        {
+            MessageBox.Show("Hello");
+                
+
+        }
+        else
+        {
+            MessageBox.Show("Error");
+
+        }
+
+
+
+     }
+    private void Register_Click(object sender, RoutedEventArgs e)
+    {
+            Register_Frame SingUp = new Register_Frame();
+            SingUp.Show();
+        }
+
+
+
+        private bool CheckLogin(String Name,String Pass)
+        {
+            String Consulta = $"select Nombre,Pass from Usuario where Nombre='"+Name+"' and Pass='"+Pass+"'";
+            String ConnectionStrig = ConfigurationManager.ConnectionStrings[StringConnection].ConnectionString;
+            using (SqlConnection Connection = new SqlConnection(ConnectionStrig))
+            {
+                Connection.Open();
+                using(SqlCommand cmd = new SqlCommand(Consulta,Connection))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    reader.Close();
+                }
+                Connection.Close();
+            }
+            
+            
+        }
+
+
+    }
+}
---
+++ TaskManager/TaskManager/Pages/Register_Frame.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Lógica de interacción para Register_Frame.xaml
+    /// </summary>
+    public partial class Register_Frame : Window
+    {
+        WindowStyle style = WindowStyle.ToolWindow;
+        public Register_Frame()
+        {
+            this.WindowStyle = style;
+            InitializeComponent();
+            String ConnectionString = ConfigurationManager.ConnectionStrings[StringConnection].ConnectionString;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Register(this.UserName.Text, this.Password.Password, this.Email.Text);
+            }
+            catch{
+                MessageBox.Show("Error inesperado");
+            }
+
+            this.Close();
+
+        }
+
+        private void Register(String Name,String Pass,String Mail)
+        {
+            String ConnectionString = ConfigurationManager.ConnectionStrings[StringConnection].ConnectionString;
+            String Consulta = $"insert into Usuario(Nombre,Pass,Correo) values('{Name}','{Pass}','{Mail}');";
+            using (DataConnection = new SqlConnection(ConnectionString))
+            {
+                DataConnection.Open();
+                if (this.Password.Password== this.ConPassword.Password){
+                    Pass=this.ConPassword.Password;
+                    using (SqlCommand cmd = new SqlCommand(Consulta, DataConnection))
+                    using (SqlDataReader RD = cmd.ExecuteReader())
+
+                    this.UserName.Clear();
+                    this.Email.Clear();
+                    this.Password.Clear();
+                    this.ConPassword.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("The pass must be the same in both Tex Boxes");
+                }
+                DataConnection.Close();
+            }
+
+        }
+
+
+        String StringConnection = "TaskManager.Properties.Settings.Task_ManagerConnectionString";
+        SqlConnection DataConnection;
+
+
+    }
+}
